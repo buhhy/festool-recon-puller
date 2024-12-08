fb53ad01f971b9389ad501d7d0640ee72c4fee1a
Terence <t3hbuhhy@gmail.com>
2024-12-08 01:50:56 -0800

    Slight fix to setup script

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,1 +1,2 @@
-virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+python3.13 -m venv .env && source .env/bin/activate && pip install -r requirements.txt
+pip install --upgrade pip
